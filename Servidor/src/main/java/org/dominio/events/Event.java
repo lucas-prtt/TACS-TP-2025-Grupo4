1304a5ec76971172f016a2df1cc860fcb488782e
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-28 20:32:30 -0300

    Event completo

--- Servidor/src/main/java/org/dominio/events/Event.java
+++ Servidor/src/main/java/org/dominio/events/Event.java
@@ -3,11 +3,14 @@ package org.dominio.events;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.dominio.usuarios.Account;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,8 +21,10 @@ public class Event {
     Integer durationMinutes;
     String location;
     Integer maxParticipants;
-    Optional<Integer> minParticipants;
+    Integer minParticipants; // Puede estar en null para ser opcional
     BigDecimal price;
     Category category;
     List<Tag> tags;
+    List<Registration> participants;
+    Queue<Account> waitList;
 }
