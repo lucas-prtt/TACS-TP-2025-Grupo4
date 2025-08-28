e149688e19628ab932ac257e94dd97d2e2f83b24
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-28 20:17:38 -0300

    Sprinboot funcionando

--- Servidor/src/main/java/org/controllers/AdminController.java
+++ Servidor/src/main/java/org/controllers/AdminController.java
@@ -1,4 +1,8 @@
 package org.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+
 }
--- Servidor/src/main/java/org/servidor/Servidor.java
+++ Servidor/src/main/java/org/servidor/Servidor.java
@@ -1,10 +1,12 @@
+package org.servidor;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Collections;
 
 
-
+@SpringBootApplication
 public class Servidor {
     public static void main(String[] args) {
         System.out.println("Servidor iniciado");
@@ -13,5 +15,6 @@ public class Servidor {
         app.setLogStartupInfo(false);
         app.setDefaultProperties(Collections.singletonMap("logging.level.root", "WARN"));
         app.run();
+        System.out.println("Springboot iniciado");
     }
 }
