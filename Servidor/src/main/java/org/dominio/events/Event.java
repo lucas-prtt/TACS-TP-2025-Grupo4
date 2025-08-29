4742996f42f2a7f36dcdee1758889c64e0781a99
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-28 21:53:15 -0300

    Creo repositorio y builder con constructores para evento

--- Servidor/src/main/java/org/dominio/events/Event.java
+++ Servidor/src/main/java/org/dominio/events/Event.java
@@ -2,19 +2,19 @@ package org.dominio.events;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.dominio.usuarios.Account;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.Queue;
+import java.util.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class Event {
+    UUID id;
     String title;
     String description;
     LocalDateTime startDateTime;
@@ -27,4 +27,38 @@ public class Event {
     List<Tag> tags;
     List<Registration> participants;
     Queue<Account> waitList;
+
+    // Constructor de Event. Requiere: String title, String description, LocalDateTime startDateTime, Integer durationMinutes, String location, Integer maxParticipants, BigDecimal price
+    public Event(String title, String description, LocalDateTime startDateTime, Integer durationMinutes, String location, Integer maxParticipants, Integer minParticipants, BigDecimal price, Category category, List<Tag> tags){
+        //Obligatorios
+        Objects.requireNonNull(title);
+        Objects.requireNonNull(description);
+        Objects.requireNonNull(startDateTime);
+        Objects.requireNonNull(durationMinutes);
+        Objects.requireNonNull(location);
+        Objects.requireNonNull(maxParticipants);
+        Objects.requireNonNull(price);
+        this.title = title;
+        this.description = description;
+        this.startDateTime = startDateTime;
+        this.durationMinutes = durationMinutes;
+        this.location = location;
+        this.maxParticipants = maxParticipants;
+        this.price = price;
+
+        //Opcionales
+        this.category = category;
+        this.minParticipants = minParticipants;
+        if(tags != null) {
+            this.tags = tags;
+        }
+        else {
+            this.tags = new ArrayList<>();
+        }
+
+        // Automaticos
+        this.participants = new ArrayList<>();
+        this.waitList = new ArrayDeque<>();
+        this.id = UUID.randomUUID();
+    }
 }
--- Servidor/src/main/java/org/repositories/EventRepository.java
+++ Servidor/src/main/java/org/repositories/EventRepository.java
@@ -1,5 +1,35 @@
 package org.repositories;
 
-public class EventRepository {
+import org.dominio.events.Event;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+public class EventRepository {
+    List<Event> events = new ArrayList<Event>();
+
+    // Recibe una uuid del evento a buscar
+    // Devuelve el evento guardado
+    // Si no hay un elemento guardado, lanza NoSuchElementException
+    public Event findById(UUID uuid) throws NoSuchElementException {
+        return events.stream().filter(event -> event.getId() == uuid).findFirst().get();
+    }
+
+    // Recibe el evento a añadir
+    // Almacena el evento en la base de datos
+    // Si esta presente uno con la misma id, lo sobreescribe
+    // Si el evento recibido es null, lanza NullPointerException
+    public void save(Event newEvent) throws NullPointerException{
+        if (newEvent == null) throw new NullPointerException("newEvent value is null");
+        Event existingEvent = findById(newEvent.getId());
+        if(existingEvent == null){
+            events.add(newEvent);
+        }
+        else {
+            events.remove(existingEvent);
+            events.add(newEvent);
+        }
+    }
 }
