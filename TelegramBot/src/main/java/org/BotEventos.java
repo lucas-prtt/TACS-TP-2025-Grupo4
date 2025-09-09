a044c6ee7ccd8c956faed5c9fd727dfdf1f0b7dd
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-09 20:20:33 -0300

    Mensajes de prueba y toma de valores de token y username desde variables de entorno

--- TelegramBot/src/main/java/org/BotEventos.java
+++ TelegramBot/src/main/java/org/BotEventos.java
@@ -1,21 +1,49 @@
 package org;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class BotEventos extends TelegramLongPollingBot {
 
-    private final String BOT_USERNAME = "TacsEventsGrupo4bot";
+    private final String BOT_USERNAME;
 
-    public BotEventos(String token) {
+    public BotEventos(String token, String botUsername) {
         super(token);
+        BOT_USERNAME = botUsername;
+
     }
 
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            String chatId = update.getMessage().getChatId().toString();
+            SendMessage response = new SendMessage();
+            response.setChatId(chatId);
+            switch (messageText){
+                case "/start":
+                    response.setText("Bot iniciado");
+                    break;
+                case "/estado":
+                    response.setText("Funcionando");
+                    break;
+                default:
+                    response.setText("Comando desconocido");
+                    break;
+            }
+
+            try {
+                execute(response); // Envía el mensaje al usuario
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
 
     }
 
