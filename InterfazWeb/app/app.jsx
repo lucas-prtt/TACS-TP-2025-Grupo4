61a31a07a38a29795278e1fa9767c5e228257e18
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-10 12:11:05 -0300

    Estructura basica carpetas

---
+++ InterfazWeb/app/app.jsx
@@ -0,0 +1,11 @@
+
+import {app.css} from "../src/styles"
+import React from "react";
+
+const App = () => {
+  return (
+            <h1 className="title">Fetching data and create custom Hook</h1>
+      <span className="subtitle">using Rick and Morty API</span>
+  )
+}
+export default App;
