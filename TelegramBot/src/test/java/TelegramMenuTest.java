7d15765483028e732b2e1cb26dbf2b3ef721309b
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-16 09:34:24 -0300

    Check keyboardMarkup

--- TelegramBot/src/test/java/TelegramMenuTest.java
+++ TelegramBot/src/test/java/TelegramMenuTest.java
@@ -50,42 +50,54 @@ public class TelegramMenuTest {
         SendMessage question1 = user.getQuestion();
         assert (response1 == null);
         assert (question1.getText().startsWith("Menu principal"));
+        assert (question1.getReplyMarkup() != null);
+
 
         Update update2 = getMockUpdate(false, "/buenosDias");
         SendMessage response2 = user.respondTo(update2);
         SendMessage question2 = user.getQuestion();
         assert (response2.getText().equals("Error - opcion invalida\n"));
         assert (question2.getText().startsWith("Menu principal"));
+        assert (question2.getReplyMarkup() != null);
+
 
         Update update3 = getMockUpdate(true, "/buenosDias");
         SendMessage response3 = user.respondTo(update3);
         SendMessage question3 = user.getQuestion();
         assert (response3.getText().equals("Error - opcion invalida\n"));
         assert (question3.getText().startsWith("Menu principal"));
+        assert (question3.getReplyMarkup() != null);
+
 
         Update update4 = getMockUpdate(true, "/organizerMenu");
         SendMessage response4 = user.respondTo(update4);
         SendMessage question4 = user.getQuestion();
         assert (response4.getText().equals("Inicia sesion antes de entrar a estos menus"));
         assert (question4.getText().startsWith("Menu principal"));
+        assert (question4.getReplyMarkup() != null);
 
         Update update5 = getMockUpdate(true, "/browseMenu");
         SendMessage response5 = user.respondTo(update5);
         SendMessage question5 = user.getQuestion();
         assert (response5.getText().equals("Inicia sesion antes de entrar a estos menus"));
         assert (question5.getText().startsWith("Menu principal"));
+        assert (question5.getReplyMarkup() != null);
 
         Update update6 = getMockUpdate(true, "/participantMenu");
         SendMessage response6 = user.respondTo(update6);
         SendMessage question6 = user.getQuestion();
         assert (response6.getText().equals("Inicia sesion antes de entrar a estos menus"));
         assert (question6.getText().startsWith("Menu principal"));
+        assert (question6.getReplyMarkup() != null);
+
 
         Update update7 = getMockUpdate(true, "/userMenu");
         SendMessage response7 = user.respondTo(update7);
         SendMessage question7 = user.getQuestion();
         assert (response7 == null);
         assert (question7.getText().contains("Menu de Usuario"));
+        assert (question7.getReplyMarkup() != null);
+
     }
 
     @Test
@@ -95,54 +107,70 @@ public class TelegramMenuTest {
         SendMessage question1 = user.getQuestion();
         assert (response1 == null);
         assert (question1.getText().startsWith("Menu principal"));
+        assert (question1.getReplyMarkup() != null);
+
 
         Update update2 = getMockUpdate(false, "/userMenu");
         SendMessage response2 = user.respondTo(update2);
         SendMessage question2 = user.getQuestion();
         assert (response2 == null);
         assert (question2.getText().contains("Menu de Usuario"));
+        assert (question2.getReplyMarkup() != null);
+
 
         Update update3 = getMockUpdate(true, "/registerUser");
         SendMessage response3 = user.respondTo(update3);
         SendMessage question3 = user.getQuestion();
         assert (response3 == null);
         assert (question3.getText().startsWith("Ingrese el nombre del usuario a crear"));
+        assert (question3.getReplyMarkup() == null);
+
 
         Update update4 = getMockUpdate(false, "Pepe");
         SendMessage response4 = user.respondTo(update4);
         SendMessage question4 = user.getQuestion();
         assert (response4 == null);
         assert (question4.getText().startsWith("Ingrese la contraseña"));
+        assert (question4.getReplyMarkup() == null);
+
 
         Update update5 = getMockUpdate(false, "Pepe123##");
         SendMessage response5 = user.respondTo(update5);
         SendMessage question5 = user.getQuestion();
         assert (response5.getText().equals("Cuenta creada\n" + "userID: " + "f6834356-d03d-4450-8df1-9644caf9fe7d" + "\n"));
         assert (question5.getText().contains("Menu principal"));
+        assert (question5.getReplyMarkup() != null);
+
 
         Update updatea = getMockUpdate(false, "/userMenu");
         SendMessage responsea = user.respondTo(updatea);
         SendMessage questiona = user.getQuestion();
         assert (responsea == null);
         assert (questiona.getText().contains("Menu de Usuario"));
+        assert (questiona.getReplyMarkup() != null);
+
 
         Update update6 = getMockUpdate(false, "/loginUser");
         SendMessage response6 = user.respondTo(update6);
         SendMessage question6 = user.getQuestion();
         assert (response6 == null);
         assert (question6.getText().startsWith("Ingrese el nombre del usuario"));
+        assert (question6.getReplyMarkup() == null);
+
 
         Update update7 = getMockUpdate(false, "Pepe");
         SendMessage response7 = user.respondTo(update7);
         SendMessage question7 = user.getQuestion();
         assert (response7 == null);
         assert (question7.getText().startsWith("Ingrese la contraseña"));
+        assert (question7.getReplyMarkup() == null);
 
         Update update8 = getMockUpdate(false, "Pepe123##");
         SendMessage response8 = user.respondTo(update8);
         SendMessage question8 = user.getQuestion();
         assert (response8.getText().equals("Cuenta Logueada\n" + "userID: " + "f6834356-d03d-4450-8df1-9644caf9fe7d" + "\n" + "username: " + "Pepe" + "\n"));
         assert (question8.getText().startsWith("Menu principal"));
+        assert (question8.getReplyMarkup() != null);
     }
 
 }
