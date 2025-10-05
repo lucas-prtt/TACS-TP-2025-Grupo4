56a042a62b55d482ee8087549ef1b1dfb935247c
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-04 22:12:18 -0300

    Mensajes de error mas amigables en login

--- TelegramBot/src/main/java/org/menus/userMenu/RegisterUserMenu.java
+++ TelegramBot/src/main/java/org/menus/userMenu/RegisterUserMenu.java
@@ -1,12 +1,18 @@
 package org.menus.userMenu;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eventServerClient.ApiClient;
 import org.eventServerClient.dtos.AccountDTO;
 import org.menus.MainMenu;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.users.TelegramUser;
 import org.menus.MenuState;
+
+import java.util.Map;
 
 public class RegisterUserMenu extends MenuState {
     AccountDTO newUser = new AccountDTO();
@@ -25,12 +31,23 @@ public class RegisterUserMenu extends MenuState {
         }catch (HttpClientErrorException e){
             System.out.println(e.getMessage());
             user.setMenu(new UserMenu(user));
-            return "Error al crear usuario." + e.getResponseBodyAsString();
+            try {
+                Map<String, String> errorMap = new ObjectMapper().readValue(e.getResponseBodyAsString(), Map.class);
+                return e.getStatusCode().toString() + "\n" + errorMap.getOrDefault("error", "Error desconocido") + "\n\n";
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+                user.setMenu(new UserMenu(user));
+                return "Error desconocido en el servidor.\n";
+            }
+        }catch (ResourceAccessException e) {
+            System.out.println("Servidor no disponible: " + e.getMessage());
+            user.setMenu(new UserMenu(user));
+            return "Error: el servidor no está disponible. Intente más tarde.";
         }
         catch (Exception e){
             System.out.println(e.getMessage());
             user.setMenu(new UserMenu(user));
-            return "Error al crear usuario." + e.getMessage();
+            return "Error interno en el bot. \n";
         }
     }
 
