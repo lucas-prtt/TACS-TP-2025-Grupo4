7dcffaaec39a77dc7018b3346b71fc069e4bd6f6
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-13 00:30:08 -0300

    Date filter con menu amigable

---
+++ TelegramBot/src/main/java/org/menus/browseMenu/FilterByDateMenu.java
@@ -0,0 +1,36 @@
+package org.menus.browseMenu;
+
+import org.menus.MenuState;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.users.QueryFilter;
+import org.users.TelegramUser;
+import org.utils.inputSteps.GenericDateTimeInputStep;
+
+public class FilterByDateMenu extends MenuState {
+    QueryFilter filter;
+    GenericDateTimeInputStep input;
+    public FilterByDateMenu(TelegramUser user, String filterParameter) {
+        super(user);
+        this.filter = new QueryFilter(filterParameter);
+        input = new GenericDateTimeInputStep("value", user);
+    }
+
+
+    @Override
+    public String respondTo(String message) {
+        if(!input.handleInput(message, filter, user))
+            return null;
+        user.addFilter(filter);
+        user.setMenu(new FilterMenu(user));
+        return user.getLocalizedMessage("configuredFilter", filter.getTypeLocalized(user), filter.getValue());
+    }
+
+    @Override
+    public String getQuestion() {
+        return " - ";
+    }
+    @Override
+    public SendMessage questionMessage() {
+        return input.getQuestion(user);
+    }
+}
