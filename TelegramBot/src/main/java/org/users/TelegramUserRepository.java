ea52f02ae6219042088a363a8af005545304e4bb
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-21 15:26:24 -0300

    Cache de TelegramUserRepository con LRU modificado y maxima cantidad de sesiones simultaneas para manejo en ram

--- TelegramBot/src/main/java/org/users/TelegramUserRepository.java
+++ TelegramBot/src/main/java/org/users/TelegramUserRepository.java
@@ -1,24 +1,46 @@
 package org.users;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class TelegramUserRepository {
 
-    private final Map<Long, TelegramUser> users = new HashMap<>();
+    private final Cache<Long, TelegramUser> users;
 
-    public TelegramUserRepository(){
+    public TelegramUserRepository() {
+        users = Caffeine.newBuilder()
+                .expireAfterAccess(3, TimeUnit.DAYS)
+                .expireAfterWrite(6, TimeUnit.DAYS)
+                .maximumSize(500_000)
+                .removalListener((Long key, TelegramUser user, RemovalCause cause) -> {
+                    System.out.println("Usuario " + key + " eliminado: " + cause);
+                })
+                .build();
     }
-    public TelegramUser addUser(Long chatId, TelegramUser user){
+
+
+
+    public TelegramUser addUser(Long chatId, TelegramUser user) {
         users.put(chatId, user);
         return user;
     }
-    public Optional<TelegramUser> getUser(Long chatid){
-        if(users.containsKey(chatid))
-            return Optional.of(users.get(chatid));
-        else return Optional.empty();
+
+    public Optional<TelegramUser> getUser(Long chatId) {
+        return Optional.ofNullable(users.getIfPresent(chatId));
     }
 
+    public void removeUser(Long chatId) {
+        users.invalidate(chatId);
+    }
+
+    public long getActiveUserCount() {
+        return users.estimatedSize();
+    }
 
 }
