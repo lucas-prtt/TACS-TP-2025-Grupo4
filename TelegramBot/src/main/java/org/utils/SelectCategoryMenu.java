792d28c40162888a2a37e542e51d881cc448f5f7
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-25 12:40:54 -0300

    Paso responsabilidad de manejo de filtros a CategorySelectMenu para poder usarlos en busquedas y creacion de filtros

--- TelegramBot/src/main/java/org/utils/SelectCategoryMenu.java
+++ TelegramBot/src/main/java/org/utils/SelectCategoryMenu.java
@@ -6,9 +6,12 @@ import lombok.Setter;
 import org.eventServerClient.dtos.event.CategoryDTO;
 import org.glassfish.jersey.internal.util.Producer;
 import org.menus.MenuState;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.users.TelegramUser;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,6 +28,33 @@ public class SelectCategoryMenu extends AbstractBrowseMenu<CategoryDTO>{
         this.onSelection = onSelection;
         this.onBack = onBack;
     }
+    @Override
+    public String respondTo(String message){
+        String fstChar = message.substring(0, 1);
+        if(isLetter(fstChar)) // No empieza con / (/page, /next, etc) ni con numero (opcion elegida)
+        {
+            setStartsWith(message);
+            return null;
+        }
+        if(message.equals("/eraseStartsWith")){
+            setStartsWith("");
+            return null;
+        }
+        return super.respondTo(message);
+    }
+    @Override
+    public SendMessage questionMessage(){
+        List<String> options = optionsList();
+        String text = getQuestion();
+        return InlineMenuBuilder.localizedMenu(user, user.getLocalizedMessage("inputGeneric", user.getLocalizedMessage("category")) + "\n" +  user.getLocalizedMessage("writeCategoryToFilter") + " \n" + text, List.of("/prev", "/next"), options, List.of("/back", "/start"), !Objects.equals(getStartsWith(), "") ?List.of("/eraseStartsWith") : List.of());
+    }
+    public SendMessage questionMessageWithoutBackAndStart(){
+        List<String> options = optionsList();
+        String text = getQuestion();
+        return InlineMenuBuilder.localizedMenu(user, user.getLocalizedMessage("inputGeneric", user.getLocalizedMessage("category")) + "\n" +  user.getLocalizedMessage("writeCategoryToFilter") + " \n" + text, List.of("/prev", "/next"), options, !Objects.equals(getStartsWith(), "") ?List.of("/eraseStartsWith") : List.of());
+    }
+
+
 
     @Override
     protected List<CategoryDTO> fetchItems(int page, int limit) {
@@ -65,4 +95,13 @@ public class SelectCategoryMenu extends AbstractBrowseMenu<CategoryDTO>{
     protected MenuState getBackMenu() {
         return onBack.call();
     }
+    private boolean isLetter(String character) {
+        return character != null
+                && character.length() == 1
+                && LETTERS.contains(character.toLowerCase());
+    }
+    private static final Set<String> LETTERS = "abcdefghijklmnñopqrstuvwxyz<"
+            .chars()
+            .mapToObj(c -> String.valueOf((char)c))
+            .collect(Collectors.toUnmodifiableSet());
 }
--- TelegramBot/src/main/java/org/utils/inputSteps/CategoryDTOInputStep.java
+++ TelegramBot/src/main/java/org/utils/inputSteps/CategoryDTOInputStep.java
@@ -19,18 +19,13 @@ import java.util.stream.Collectors;
 public class CategoryDTOInputStep implements EventInputStep{
 
     private final String fieldName;
-    private final String label;
     private boolean finished = false;
     private String category = null;
     private final SelectCategoryMenu selectMenu;
-    private static final Set<String> LETTERS = "abcdefghijklmnñopqrstuvwxyz<"
-            .chars()
-            .mapToObj(c -> String.valueOf((char)c))
-            .collect(Collectors.toUnmodifiableSet());
 
-    public CategoryDTOInputStep(String fieldName, String label, TelegramUser user) {
+
+    public CategoryDTOInputStep(String fieldName, TelegramUser user) {
         this.fieldName = fieldName;
-        this.label = label;
         this.selectMenu = new SelectCategoryMenu(user,
                 (categoryDTO) -> {
                     this.category = categoryDTO.getTitle();
@@ -41,24 +36,12 @@ public class CategoryDTOInputStep implements EventInputStep{
 
     @Override
     public SendMessage getQuestion(TelegramUser user) {
-        List<String> options = selectMenu.optionsList();
-        String text = selectMenu.getQuestion();
-        return InlineMenuBuilder.localizedMenu(user, label.transform(I18nManager::capitalize) + "\n" +  user.getLocalizedMessage("writeCategoryToFilter") + " \n" + text, List.of("/prev", "/next"), options, !Objects.equals(selectMenu.getStartsWith(), "") ?List.of("/eraseStartsWith") : List.of());
+        return selectMenu.questionMessageWithoutBackAndStart();
     }
 
     @Override
     public boolean handleInput(String message, EventDTO eventDTO, TelegramUser user) {
         try {
-            String fstChar = message.substring(0, 1);
-            if(isLetter(fstChar)) // No empieza con / (/page, /next, etc) ni con numero (opcion elegida)
-            {
-                selectMenu.setStartsWith(String.copyValueOf(message.toCharArray()));
-                return false;
-            }
-            if(message.equals("/eraseStartsWith")){
-                selectMenu.setStartsWith("");
-                return false;
-            }
             selectMenu.respondTo(message);
             if(finished){
                 Field field = eventDTO.getClass().getDeclaredField(fieldName);
@@ -70,10 +53,6 @@ public class CategoryDTOInputStep implements EventInputStep{
             return false;
         }
     }
-    private boolean isLetter(String character) {
-        return character != null
-                && character.length() == 1
-                && LETTERS.contains(character.toLowerCase());
-    }
+
 
 }
