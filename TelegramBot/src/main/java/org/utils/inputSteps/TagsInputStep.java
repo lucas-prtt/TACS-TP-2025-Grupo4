a0b76d89fb2083a340ed75c7e5f6f96f06e7b0aa
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-26 20:04:54 -0300

    Botones atras/cancelar al crear eventos

--- TelegramBot/src/main/java/org/utils/inputSteps/TagsInputStep.java
+++ TelegramBot/src/main/java/org/utils/inputSteps/TagsInputStep.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.eventServerClient.dtos.event.EventDTO;
+import org.eventServerClient.dtos.event.TagDTO;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.users.TelegramUser;
 import org.utils.InlineMenuBuilder;
@@ -33,9 +34,7 @@ public class TagsInputStep implements EventInputStep{
         try {
             if(Objects.equals(message, "/stop"))
                 return true;
-            Field field = eventDTO.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(eventDTO, message);
+            eventDTO.getTags().add(new TagDTO(message));
             return false;
         } catch (Exception e) {
             return false;
