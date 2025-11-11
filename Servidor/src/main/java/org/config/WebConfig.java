af951df2200b9ca89b7f1f3186d340e9a3028cf3
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-11-11 20:16:39 -0300

    CORS con ip publica de la instancia

--- Servidor/src/main/java/org/config/WebConfig.java
+++ Servidor/src/main/java/org/config/WebConfig.java
@@ -4,16 +4,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String ip = getPublicIP();
+        String frontendOrigin = "http://" + ip + ":" + 3000;
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000", frontendOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    public static String getPublicIP() {
+        try {
+            URL url = new URL("http://checkip.amazonaws.com");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            return br.readLine();
+        } catch (Exception e) {
+            return "localhost";
+        }
     }
 }
